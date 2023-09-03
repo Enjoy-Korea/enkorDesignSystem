1112c57432a456e60a33f931cf754a0f1978c8e9
marc-enkor <jungsu.jin@enkor.kr>
2023-09-03 12:00:48 +0900

    fix : config

---
+++ build/index.esm.js
@@ -0,0 +1,62 @@
+import React from 'react';
+import styled from 'styled-components';
+
+/******************************************************************************
+Copyright (c) Microsoft Corporation.
+
+Permission to use, copy, modify, and/or distribute this software for any
+purpose with or without fee is hereby granted.
+
+THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES WITH
+REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF MERCHANTABILITY
+AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR ANY SPECIAL, DIRECT,
+INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES WHATSOEVER RESULTING FROM
+LOSS OF USE, DATA OR PROFITS, WHETHER IN AN ACTION OF CONTRACT, NEGLIGENCE OR
+OTHER TORTIOUS ACTION, ARISING OUT OF OR IN CONNECTION WITH THE USE OR
+PERFORMANCE OF THIS SOFTWARE.
+***************************************************************************** */
+/* global Reflect, Promise */
+
+
+var __assign = function() {
+    __assign = Object.assign || function __assign(t) {
+        for (var s, i = 1, n = arguments.length; i < n; i++) {
+            s = arguments[i];
+            for (var p in s) if (Object.prototype.hasOwnProperty.call(s, p)) t[p] = s[p];
+        }
+        return t;
+    };
+    return __assign.apply(this, arguments);
+};
+
+function __rest(s, e) {
+    var t = {};
+    for (var p in s) if (Object.prototype.hasOwnProperty.call(s, p) && e.indexOf(p) < 0)
+        t[p] = s[p];
+    if (s != null && typeof Object.getOwnPropertySymbols === "function")
+        for (var i = 0, p = Object.getOwnPropertySymbols(s); i < p.length; i++) {
+            if (e.indexOf(p[i]) < 0 && Object.prototype.propertyIsEnumerable.call(s, p[i]))
+                t[p[i]] = s[p[i]];
+        }
+    return t;
+}
+
+function __makeTemplateObject(cooked, raw) {
+    if (Object.defineProperty) { Object.defineProperty(cooked, "raw", { value: raw }); } else { cooked.raw = raw; }
+    return cooked;
+}
+
+var StyledButton = styled.button.withConfig({
+  displayName: "Button__StyledButton",
+  componentId: "sc-1vdts62-0"
+})(templateObject_1 || (templateObject_1 = __makeTemplateObject(["\n  font-weight: 600;\n  border: none;\n  border-radius: 20px;\n  cursor: pointer;\n  display: inline-block;\n  color: #515151;\n  background-color: #ffd951;\n  padding: 10px 20px;\n  &:hover {\n    background-color: #ffb801;\n  }\n"], ["\n  font-weight: 600;\n  border: none;\n  border-radius: 20px;\n  cursor: pointer;\n  display: inline-block;\n  color: #515151;\n  background-color: #ffd951;\n  padding: 10px 20px;\n  &:hover {\n    background-color: #ffb801;\n  }\n"])));
+var Button = function Button(_a) {
+  _a.label;
+    var children = _a.children,
+    rest = __rest(_a, ["label", "children"]);
+  return /*#__PURE__*/React.createElement(StyledButton, __assign({}, rest), children);
+};
+var templateObject_1;
+
+export { Button };
+//# sourceMappingURL=index.esm.js.map
--- rollup.config.js
+++ rollup.config.js
@@ -4,54 +4,40 @@ import babel from "@rollup/plugin-babel";
 import pkg from "./package.json" assert { type: "json" };
 import peerDepsExternal from "rollup-plugin-peer-deps-external";
 import typescript from "@rollup/plugin-typescript";
-// import { terser } from "rollup-plugin-terser";
+// import { terser } from "rollup-plugin-terser"; // 배포할 때 주석 해제 권장
 
 const extensions = [".js", ".jsx", ".ts", ".tsx"];
 const external = ["react", "react-dom", "styled-components", "classNames"];
-
-process.env.BABEL_ENV = "production";
 
 export default {
   input: "src/index.ts",
   output: [
     {
-      file: pkg.main, // (package.json) main 경로로 번들링.
-      format: "cjs", // cjs로 번들링
+      file: pkg.main, // package.json의 main 경로로 번들링
+      format: "cjs",
       sourcemap: true,
     },
     {
-      file: pkg.module, // (package.json) main 경로로 번들링.
-      format: "esm", // es로 변들링
+      file: pkg.module, // package.json의 module 경로로 번들링
+      format: "esm",
       sourcemap: true,
     },
   ],
 
   plugins: [
-    // typescript(),
-
     typescript({
-      // useTsconfigDeclarationDir: true,
       tsconfig: "./tsconfig.json",
     }),
-
-    peerDepsExternal(), //peerDependencies에 설치된 라이브러리들을 external모듈로 설정하여 번들 결과물에서 제외
-    nodeResolve({ extensions }), //node_modules에서 모듈을 불러올 수 있도록 만드는 플러그인
-    commonjs({ extensions }), // commonJS로 만들어진 모듈을 사용 가능하게 하는 플러그인
-    // babel({
-    //   extensions,
-    //   include: ["src/**/*"],
-    //   babelHelpers: "bundled",
-    //   exclude: /node_modules/,
-    //   skipPreflightCheck: true,
-    // }),
-
+    peerDepsExternal(), // peerDependencies에 설치된 라이브러리를 external 모듈로 설정하여 번들 결과물에서 제외
+    nodeResolve({ extensions }), // node_modules에서 모듈을 불러올 수 있게 하는 플러그인
+    commonjs({ extensions }), // commonJS로 작성된 모듈을 사용 가능하게 하는 플러그인
     babel({
       babelHelpers: "bundled",
-      exclude: "node_modules/**/*.(ts|tsx|js|jsx)",
-      include: "src/**/*.(ts|tsx|js|jsx)",
-      extensions: [".ts", ".tsx", ".js", ".jsx", ".es", ".es6", ".mjs"],
+      exclude: ["node_modules/**", "dist/**"],
+      include: ["src/**/*"],
+      extensions,
     }),
-    // terser(),
+    // terser(), // 배포할 때 주석 해제 권장
   ],
   external,
 };
