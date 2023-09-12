a4dd13a78e979e88e139630c29b7d56a5864a0ca
Sia2304 <kyoorim.lee@enkor.kr>
2023-09-12 16:08:27 +0900

    add:color related files

---
+++ src/color/color.ts
@@ -0,0 +1,10 @@
+export const grayScale = {
+  black: "#000000",
+  300: "#333333",
+  main: "#515151",
+  500: "#8B8B8B",
+  700: "#C1C1C1",
+  800: "#DDDDDD",
+  900: "#ECECEC",
+  1000: "#F5F5F5",
+};
