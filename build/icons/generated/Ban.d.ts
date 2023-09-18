6c48d61a8382064a70454ae592536838a82680a5
marc-enkor <jungsu.jin@enkor.kr>
2023-09-19 08:18:55 +0900

    delete : unused icon

--- build/icons/generated/Ban.d.ts
+++
@@ -1,3 +0,0 @@
-import React from "react";
-import type { IconProps } from "../types";
-export declare const BanIcon: ({ size, fillColor, strokeColor, className, style, viewBox }: IconProps) => React.JSX.Element;
