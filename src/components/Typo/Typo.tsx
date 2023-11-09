e087e3c2a6fc562d14e3db4e4e1540a67ea24ee1
marc-enkor <jungsu.jin@enkor.kr>
2023-11-09 14:12:06 +0900

    add : typo mobile size

--- src/components/Typo/Typo.tsx
+++ src/components/Typo/Typo.tsx
@@ -50,12 +50,51 @@ export const Heading = ({
     },
   };
 
+  const filterMobileHeadingStyle = {
+    h1: {
+      size: "32px",
+      lineHeight: "40px",
+      weight: "700",
+    },
+    h2: {
+      size: "28px",
+      lineHeight: "36px",
+      weight: "700",
+    },
+    h3: {
+      size: "24px",
+      lineHeight: "32px",
+      weight: "700",
+    },
+    h4: {
+      size: "22px",
+      lineHeight: "28px",
+      weight: "600",
+    },
+    h5: {
+      size: "18px",
+      lineHeight: "24px",
+      weight: "600",
+    },
+    h6: {
+      size: "16px",
+      lineHeight: "22px",
+      weight: "600",
+    },
+  };
+
   const StyledHeading = styled[name]`
     font-size: ${filterHeadingStyle[name].size};
     line-height: ${filterHeadingStyle[name].lineHeight};
     font-weight: ${filterHeadingStyle[name].weight};
     color: ${color};
     margin: 0;
+
+    @media screen and (max-width: 1024px) {
+      font-size: ${filterMobileHeadingStyle[name].size};
+      line-height: ${filterMobileHeadingStyle[name].lineHeight};
+      font-weight: ${filterMobileHeadingStyle[name].weight};
+    }
   `;
 
   return <StyledHeading {...rest}>{children}</StyledHeading>;
@@ -132,12 +171,66 @@ export const Text = ({
     },
   };
 
+  const filterMobileTextStyle = {
+    strongM: {
+      size: "16px",
+      lineHeight: "22px",
+      weight: "600",
+    },
+    strongS: {
+      size: "14px",
+      lineHeight: "20px",
+      weight: "600",
+    },
+    l: {
+      size: "16px",
+      lineHeight: "26px",
+      weight: "400",
+    },
+    spacedM: {
+      size: "16px",
+      lineHeight: "26px",
+      weight: "400",
+    },
+    m: {
+      size: "16px",
+      lineHeight: "22px",
+      weight: "400",
+    },
+    spacedS: {
+      size: "14px",
+      lineHeight: "22px",
+      weight: "400",
+    },
+    s: {
+      size: "14px",
+      lineHeight: "20px",
+      weight: "400",
+    },
+    xs: {
+      size: "12px",
+      lineHeight: "16px",
+      weight: "400",
+    },
+    xxs: {
+      size: "10px",
+      lineHeight: "14px",
+      weight: "400",
+    },
+  };
+
   const StyledText = styled[element]`
     font-size: ${filterTextStyle[name].size};
     line-height: ${filterTextStyle[name].lineHeight};
     font-weight: ${filterTextStyle[name].weight};
     color: ${color};
     margin: 0;
+    
+    @media screen and (max-width: 1024px) {
+      font-size: ${filterMobileTextStyle[name].size};
+      line-height: ${filterMobileTextStyle[name].lineHeight};
+      font-weight: ${filterMobileTextStyle[name].weight};
+    }
   `;
 
   return <StyledText {...rest}>{children}</StyledText>;
--- src/icons/generated/MessageIcon.tsx
+++ src/icons/generated/MessageIcon.tsx
@@ -9,4 +9,4 @@ export const MessageIcon = ({
   className = "",
   style = {},
   viewBox = "0 0 24 24"
-}: IconProps) => <svg xmlns="http://www.w3.org/2000/svg" width={size} height={size} fill={fillColor} style={style} className={className} viewBox={viewBox}><path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M5.422 12.275v6.43l2.995-2.756L12.695 21 22 3 2 8.602l3.422 3.673Zm0 0 7.273-3.673" stroke={strokeColor} /></svg>;
+}: IconProps) => <svg xmlns="http://www.w3.org/2000/svg" width={size} height={size} fill={fillColor} style={style} className={className} viewBox={viewBox}><path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M5.422 12.276v6.428l2.995-2.755L12.695 21 22 3 2 8.602l3.422 3.673Zm0 0 7.273-3.674" stroke={strokeColor} /></svg>;
