713d7f826e79caac31af91ba3147a6d837ee2be5
marc-enkor <jungsu.jin@enkor.kr>
2023-11-20 13:23:04 +0900

    fix : typo component rollback

--- build/components/Typo/Typo.d.ts
+++ build/components/Typo/Typo.d.ts
@@ -4,59 +4,14 @@ export interface HeadingProps extends PropsWithChildren<HTMLAttributes<HTMLHeadi
     name: "h1" | "h2" | "h3" | "h4" | "h5" | "h6";
     color?: ColorType;
 }
-export declare const Heading: ({ name, color, children, ...rest }: HeadingProps) => React.JSX.Element | null;
+export declare const Heading: ({ name, color, children, ...rest }: HeadingProps) => React.JSX.Element;
 export interface TextProps extends PropsWithChildren<HTMLAttributes<HTMLElement>> {
     name: "strongM" | "strongS" | "l" | "spacedM" | "m" | "spacedS" | "s" | "xs" | "xxs";
     color?: ColorType;
     element?: "span" | "p";
 }
-export declare const Text: ({ name, color, children, element, ...rest }: TextProps) => React.JSX.Element | null;
-/**
- *  @Heading desktop(데스크탑 환경)
- *    - h1: { size: 36px, lineHeight: 44px, weight: 700 }
- *    - h2: { size: 32px, lineHeight: 40px, weight: 700 }
- *    - h3: { size: 28px, lineHeight: 36px, weight: 700 }
- *    - h4: { size: 24px, lineHeight: 32px, weight: 600 }
- *    - h5: { size: 20px, lineHeight: 28px, weight: 600 }
- *    - h6: { size: 18px, lineHeight: 24px, weight: 600 }
-
- *  -------------------------------------------------------------------
-
- *  @Heading mobile(모바일 환경)
- *    - h1: { size: 32px, lineHeight: 40px, weight: 700 }
- *    - h2: { size: 28px, lineHeight: 36px, weight: 700 }
- *    - h3: { size: 24px, lineHeight: 32px, weight: 700 }
- *    - h4: { size: 22px, lineHeight: 28px, weight: 600 }
- *    - h5: { size: 18px, lineHeight: 24px, weight: 600 }
- *    - h6: { size: 16px, lineHeight: 22px, weight: 600 }
- *
- *  -------------------------------------------------------------------
- *
- *  @Text desktop(데스크탑 환경)
- *    - strongM: { size: 16px, lineHeight: 22px, weight: 600 }
- *    - strongS: { size: 14px, lineHeight: 20px, weight: 600 }
- *    - spacedM: { size: 16px, lineHeight: 26px, weight: 400 }
- *    - spacedS: { size: 14px, lineHeight: 22px, weight: 400 }
- *    - l: { size: 18px, lineHeight: 30px, weight: 400 }
- *    - m: { size: 16px, lineHeight: 22px, weight: 400 }
- *    - s: { size: 14px, lineHeight: 20px, weight: 400 }
- *    - xs: { size: 12px, lineHeight: 16px, weight: 400 }
- *    - xxs: { size: 10px, lineHeight: 14px, weight: 400 }
- *
- *  -------------------------------------------------------------------
- *
- *  @Text mobile(모바일 환경)
- *    - strongM: { size: 16px, lineHeight: 22px, weight: 600 }
- *    - strongS: { size: 14px, lineHeight: 20px, weight: 600 }
- *    - spacedM: { size: 16px, lineHeight: 26px, weight: 400 }
- *    - spacedS: { size: 14px, lineHeight: 22px, weight: 400 }
- *    - l: { size: 16px, lineHeight: 26px, weight: 400 }
- *    - m: { size: 16px, lineHeight: 22px, weight: 400 }
- *    - s: { size: 14px, lineHeight: 20px, weight: 400 }
- *    - xs: { size: 12px, lineHeight: 16px, weight: 400 }
- *    - xxs: { size: 10px, lineHeight: 14px, weight: 400 }
- */
+export declare const Text: ({ name, color, children, element, ...rest }: TextProps) => React.JSX.Element;
 export declare const Typo: {
-    Heading: ({ name, color, children, ...rest }: HeadingProps) => React.JSX.Element | null;
-    Text: ({ name, color, children, element, ...rest }: TextProps) => React.JSX.Element | null;
+    Heading: ({ name, color, children, ...rest }: HeadingProps) => React.JSX.Element;
+    Text: ({ name, color, children, element, ...rest }: TextProps) => React.JSX.Element;
 };
--- src/components/Typo/Typo.tsx
+++ src/components/Typo/Typo.tsx
@@ -5,47 +5,11 @@ import styled from "styled-components";
 import { ColorType } from "../../colors/types";
 import { grayScale } from "../../colors";
 
-// ------------------------------------------------------------
-
 export interface HeadingProps
   extends PropsWithChildren<HTMLAttributes<HTMLHeadingElement>> {
   name: "h1" | "h2" | "h3" | "h4" | "h5" | "h6";
   color?: ColorType;
 }
-
-const desktopHeadingStyle = {
-  h1: { size: "36px", lineHeight: "44px", weight: "700" },
-  h2: { size: "32px", lineHeight: "40px", weight: "700" },
-  h3: { size: "28px", lineHeight: "36px", weight: "700" },
-  h4: { size: "24px", lineHeight: "32px", weight: "600" },
-  h5: { size: "20px", lineHeight: "28px", weight: "600" },
-  h6: { size: "18px", lineHeight: "24px", weight: "600" },
-};
-
-const mobileHeadingStyle = {
-  h1: { size: "32px", lineHeight: "40px", weight: "700" },
-  h2: { size: "28px", lineHeight: "36px", weight: "700" },
-  h3: { size: "24px", lineHeight: "32px", weight: "700" },
-  h4: { size: "22px", lineHeight: "28px", weight: "600" },
-  h5: { size: "18px", lineHeight: "24px", weight: "600" },
-  h6: { size: "16px", lineHeight: "22px", weight: "600" },
-};
-
-const createStyledHeading = (name: HeadingProps["name"]) => styled[name]`
-  font-size: ${desktopHeadingStyle[name].size};
-  line-height: ${desktopHeadingStyle[name].lineHeight};
-  font-weight: ${desktopHeadingStyle[name].weight};
-
-  color: ${(props) => props.color || grayScale.main};
-
-  margin: 0;
-
-  @media screen and (max-width: 1024px) {
-    font-size: ${mobileHeadingStyle[name].size};
-    line-height: ${mobileHeadingStyle[name].lineHeight};
-    font-weight: ${mobileHeadingStyle[name].weight};
-  }
-`;
 
 export const Heading = ({
   name,
@@ -53,18 +17,92 @@ export const Heading = ({
   children,
   ...rest
 }: HeadingProps) => {
-  const StyledHeading = createStyledHeading(name);
+  const filterHeadingStyle = {
+    h1: {
+      size: "36px",
+      lineHeight: "44px",
+      weight: "700",
+    },
+    h2: {
+      size: "32px",
+      lineHeight: "40px",
+      weight: "700",
+    },
+    h3: {
+      size: "28px",
+      lineHeight: "36px",
+      weight: "700",
+    },
+    h4: {
+      size: "24px",
+      lineHeight: "32px",
+      weight: "600",
+    },
+    h5: {
+      size: "20px",
+      lineHeight: "28px",
+      weight: "600",
+    },
+    h6: {
+      size: "18px",
+      lineHeight: "24px",
+      weight: "600",
+    },
+  };
 
-  if (!StyledHeading) return null;
+  const filterMobileHeadingStyle = {
+    h1: {
+      size: "32px",
+      lineHeight: "40px",
+      weight: "700",
+    },
+    h2: {
+      size: "28px",
+      lineHeight: "36px",
+      weight: "700",
+    },
+    h3: {
+      size: "24px",
+      lineHeight: "32px",
+      weight: "700",
+    },
+    h4: {
+      size: "22px",
+      lineHeight: "28px",
+      weight: "600",
+    },
+    h5: {
+      size: "18px",
+      lineHeight: "24px",
+      weight: "600",
+    },
+    h6: {
+      size: "16px",
+      lineHeight: "22px",
+      weight: "600",
+    },
+  };
+
+  const StyledHeading = styled[name]`
+    font-size: ${filterHeadingStyle[name].size};
+    line-height: ${filterHeadingStyle[name].lineHeight};
+    font-weight: ${filterHeadingStyle[name].weight};
+    color: ${color};
+    margin: 0;
+
+    @media screen and (max-width: 1024px) {
+      font-size: ${filterMobileHeadingStyle[name].size};
+      line-height: ${filterMobileHeadingStyle[name].lineHeight};
+      font-weight: ${filterMobileHeadingStyle[name].weight};
+    }
+  `;
 
   return (
-    <StyledHeading color={color} {...rest}>
+    <StyledHeading {...rest} id="test-heading-1">
       {children}
     </StyledHeading>
   );
 };
-
-// ------------------------------------------------------------
 
 export interface TextProps
   extends PropsWithChildren<HTMLAttributes<HTMLElement>> {
@@ -82,50 +120,6 @@ export interface TextProps
   element?: "span" | "p";
 }
 
-const desktopTextStyle = {
-  strongM: { size: "16px", lineHeight: "22px", weight: "600" },
-  strongS: { size: "14px", lineHeight: "20px", weight: "600" },
-  spacedM: { size: "16px", lineHeight: "26px", weight: "400" },
-  spacedS: { size: "14px", lineHeight: "22px", weight: "400" },
-  l: { size: "18px", lineHeight: "30px", weight: "400" },
-  m: { size: "16px", lineHeight: "22px", weight: "400" },
-  s: { size: "14px", lineHeight: "20px", weight: "400" },
-  xs: { size: "12px", lineHeight: "16px", weight: "400" },
-  xxs: { size: "10px", lineHeight: "14px", weight: "400" },
-};
-
-const mobileTextStyle = {
-  strongM: { size: "16px", lineHeight: "22px", weight: "600" },
-  strongS: { size: "14px", lineHeight: "20px", weight: "600" },
-  spacedM: { size: "16px", lineHeight: "26px", weight: "400" },
-  spacedS: { size: "14px", lineHeight: "22px", weight: "400" },
-  l: { size: "16px", lineHeight: "26px", weight: "400" },
-  m: { size: "16px", lineHeight: "22px", weight: "400" },
-  s: { size: "14px", lineHeight: "20px", weight: "400" },
-  xs: { size: "12px", lineHeight: "16px", weight: "400" },
-  xxs: { size: "10px", lineHeight: "14px", weight: "400" },
-};
-
-const createStyledText = (
-  name: TextProps["name"],
-  element: TextProps["element"],
-  color: TextProps["color"],
-) => styled[element as "span" | "p"]`
-  font-size: ${desktopTextStyle[name].size};
-  line-height: ${desktopTextStyle[name].lineHeight};
-  font-weight: ${desktopTextStyle[name].weight};
-
-  color: ${color ?? grayScale.main};
-
-  margin: 0;
-
-  @media screen and (max-width: 1024px) {
-    font-size: ${mobileTextStyle[name].size};
-    line-height: ${mobileTextStyle[name].lineHeight};
-    font-weight: ${mobileTextStyle[name].weight};
-  }
-`;
-
 export const Text = ({
   name,
   color,
@@ -133,60 +127,122 @@ export const Text = ({
   element = "p",
   ...rest
 }: TextProps) => {
-  const StyledText = createStyledText(name, element, color);
+  const filterTextStyle = {
+    strongM: {
+      size: "16px",
+      lineHeight: "22px",
+      weight: "600",
+    },
+    strongS: {
+      size: "14px",
+      lineHeight: "20px",
+      weight: "600",
+    },
+    l: {
+      size: "18px",
+      lineHeight: "30px",
+      weight: "400",
+    },
+    spacedM: {
+      size: "16px",
+      lineHeight: "26px",
+      weight: "400",
+    },
+    m: {
+      size: "16px",
+      lineHeight: "22px",
+      weight: "400",
+    },
+    spacedS: {
+      size: "14px",
+      lineHeight: "22px",
+      weight: "400",
+    },
+    s: {
+      size: "14px",
+      lineHeight: "20px",
+      weight: "400",
+    },
+    xs: {
+      size: "12px",
+      lineHeight: "16px",
+      weight: "400",
+    },
+    xxs: {
+      size: "10px",
+      lineHeight: "14px",
+      weight: "400",
+    },
+  };
 
-  if (!StyledText) return null;
+  const filterMobileTextStyle = {
+    strongM: {
+      size: "16px",
+      lineHeight: "22px",
+      weight: "600",
+    },
+    strongS: {
+      size: "14px",
+      lineHeight: "20px",
+      weight: "600",
+    },
+    l: {
+      size: "16px",
+      lineHeight: "26px",
+      weight: "400",
+    },
+    spacedM: {
+      size: "16px",
+      lineHeight: "26px",
+      weight: "400",
+    },
+    m: {
+      size: "16px",
+      lineHeight: "22px",
+      weight: "400",
+    },
+    spacedS: {
+      size: "14px",
+      lineHeight: "22px",
+      weight: "400",
+    },
+    s: {
+      size: "14px",
+      lineHeight: "20px",
+      weight: "400",
+    },
+    xs: {
+      size: "12px",
+      lineHeight: "16px",
+      weight: "400",
+    },
+    xxs: {
+      size: "10px",
+      lineHeight: "14px",
+      weight: "400",
+    },
+  };
 
-  return <StyledText {...rest}>{children}</StyledText>;
+  const StyledText = styled[element]`
+    font-size: ${filterTextStyle[name].size};
+    line-height: ${filterTextStyle[name].lineHeight};
+    font-weight: ${filterTextStyle[name].weight};
+    color: ${color};
+    margin: 0;
+
+    @media screen and (max-width: 1024px) {
+      font-size: ${filterMobileTextStyle[name].size};
+      line-height: ${filterMobileTextStyle[name].lineHeight};
+      font-weight: ${filterMobileTextStyle[name].weight};
+    }
+  `;
+
+  return (
+    <StyledText {...rest} id="test-text-1">
+      {children}
+    </StyledText>
+  );
 };
-
-// ------------------------------------------------------------
-
-/**
- *  @Heading desktop(데스크탑 환경)
- *    - h1: { size: 36px, lineHeight: 44px, weight: 700 }
- *    - h2: { size: 32px, lineHeight: 40px, weight: 700 }
- *    - h3: { size: 28px, lineHeight: 36px, weight: 700 }
- *    - h4: { size: 24px, lineHeight: 32px, weight: 600 }
- *    - h5: { size: 20px, lineHeight: 28px, weight: 600 }
- *    - h6: { size: 18px, lineHeight: 24px, weight: 600 }
-
- *  -------------------------------------------------------------------
-
- *  @Heading mobile(모바일 환경)
- *    - h1: { size: 32px, lineHeight: 40px, weight: 700 }
- *    - h2: { size: 28px, lineHeight: 36px, weight: 700 }
- *    - h3: { size: 24px, lineHeight: 32px, weight: 700 }
- *    - h4: { size: 22px, lineHeight: 28px, weight: 600 }
- *    - h5: { size: 18px, lineHeight: 24px, weight: 600 }
- *    - h6: { size: 16px, lineHeight: 22px, weight: 600 }
- * 
- *  -------------------------------------------------------------------
- * 
- *  @Text desktop(데스크탑 환경)
- *    - strongM: { size: 16px, lineHeight: 22px, weight: 600 }
- *    - strongS: { size: 14px, lineHeight: 20px, weight: 600 }
- *    - spacedM: { size: 16px, lineHeight: 26px, weight: 400 }
- *    - spacedS: { size: 14px, lineHeight: 22px, weight: 400 }
- *    - l: { size: 18px, lineHeight: 30px, weight: 400 }
- *    - m: { size: 16px, lineHeight: 22px, weight: 400 }
- *    - s: { size: 14px, lineHeight: 20px, weight: 400 }
- *    - xs: { size: 12px, lineHeight: 16px, weight: 400 }
- *    - xxs: { size: 10px, lineHeight: 14px, weight: 400 }
- *
- *  -------------------------------------------------------------------
- *
- *  @Text mobile(모바일 환경)
- *    - strongM: { size: 16px, lineHeight: 22px, weight: 600 }
- *    - strongS: { size: 14px, lineHeight: 20px, weight: 600 }
- *    - spacedM: { size: 16px, lineHeight: 26px, weight: 400 }
- *    - spacedS: { size: 14px, lineHeight: 22px, weight: 400 }
- *    - l: { size: 16px, lineHeight: 26px, weight: 400 }
- *    - m: { size: 16px, lineHeight: 22px, weight: 400 }
- *    - s: { size: 14px, lineHeight: 20px, weight: 400 }
- *    - xs: { size: 12px, lineHeight: 16px, weight: 400 }
- *    - xxs: { size: 10px, lineHeight: 14px, weight: 400 }
- */
 
 export const Typo = {
   Heading,
