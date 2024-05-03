0cf26bedcd3d7d517d160154930e04f27507453f
JISUYOO <jisu.yoo@enkor.kr>
2024-05-03 10:32:49 +0900

    add: mail icon and usage icon

---
+++ src/icons/generated/UsageIcon.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import type { IconProps } from "../types";
+export const UsageIcon = ({
+  size = 24,
+  fillColor = "none",
+  strokeColor = "#515151",
+  className = "",
+  style = {},
+  viewBox = "0 0 24 24"
+}: IconProps) => <svg xmlns="http://www.w3.org/2000/svg" width={size} height={size} fill={fillColor} style={style} className={className} viewBox={viewBox}><path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M16 8v8m-4-5v5m-4-2v2m-2 4h12a2 2 0 0 0 2-2V6a2 2 0 0 0-2-2H6a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2Z" stroke={strokeColor} /></svg>;
